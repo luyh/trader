cb686944496b9c20c95245e755d5f79f902546a8
luyh <hebbelu@icloud.com>
2019-11-25 23:31:20 +0800

    add environment

---
+++ neat_stragtegy/environment.py
@@ -0,0 +1,86 @@
+import pandas as pd
+data_file ='../data/processed/binance/btc_usdt_5m.csv'
+df = pd.read_csv(data_file, index_col=[0])
+
+import os,sys
+if sys.platform == 'win32' or sys.platform == 'darwin':
+    parralle = True
+    num_workers = 2
+    # number of days we want to pull from the dataframe
+    days_of_data = 7
+    pop_size = 10
+    days = 1
+    generations = 20
+else:
+    parralle = False
+    num_workers = 10
+    days_of_data = 365
+    pop_size = 50
+    days = 7
+    generations = 100
+
+print('days_of_data={},pop_size = {},days = {},generations = {},'.format( days_of_data,pop_size ,days,generations))
+# number of data frames (our DF is in 1h timesteps)
+frames = days_of_data * 24 * 12
+# frames = len(df)
+train_test_percentage = 0.4
+
+x_train = int(frames * (1 - train_test_percentage))
+x_test = int(frames - x_train)
+
+df_train = df[-frames:(-x_test - 1)]
+df_test = df[-x_test:]
+print("Friend Shape", df.shape)
+print('train shape', df_train.shape)
+print('test shape', df_test.shape)
+print('columns', df.columns)
+
+print(df_test.head())
+
+del df
+
+import tensortrade
+from neat_stragtegy.neat_trading_strategy import NeatTradingStrategy as TradingStrategy
+from neat_stragtegy.neat_reward_strategy import NeatRewardStrategy as ProfitStrategy
+
+from tensortrade.actions import DiscreteActions
+from tensortrade.exchanges.simulated import SimulatedExchange as Exchange
+from tensortrade.features.scalers import MinMaxNormalizer
+from tensortrade.features import FeaturePipeline
+from tensortrade.environments import TradingEnvironment as Environment
+
+print('fin imports')
+normalize = MinMaxNormalizer(inplace=True,
+                             input_min = 2000,
+                             input_max = 20000)
+feature_pipeline = FeaturePipeline(steps=[normalize])
+
+reward_scheme = ProfitStrategy()
+action_scheme = DiscreteActions(n_actions=5, instrument='BTC/USDT')
+print('fin strats')
+
+exchange = Exchange(data_frame=df_train,
+                    pretransform = True,
+                    base_instrument='USDT',
+                    commission_percent=0.075,
+                    initial_balance = 100,
+                    window_size=1,
+                    max_allowed_slippage_percent=3.0,
+                    min_order_amount=1E-4,
+                    min_trade_amount=1E-4,
+                    observation_columns = df_train.columns
+                   )
+
+print('fin exchange')
+
+environment = Environment(exchange=exchange,
+                                 action_scheme=action_scheme,
+                                 reward_scheme=reward_scheme,
+                                 feature_pipeline=feature_pipeline)
+
+print('fin environment')
+print('')
+
+environment.exchange._balance = 200
+
+print(exchange.balance,environment.exchange._balance)
