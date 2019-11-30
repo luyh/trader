ce88c8a84b4795f71948239431cf3e75435f1968
luyh <yanhonglu5@icloud.com>
2019-11-30 16:24:01 +0800

    debug evolve

--- evolve.py
+++ evolve.py
@@ -1,28 +1,9 @@
 import pandas as pd
-data_file ='../data/processed/binance/btc_usdt_5m.csv'
-df = pd.read_csv(data_file, index_col=[0])
+data_file ='./data/processed/binance/btc_usdt_5m.csv'
+df = pd.read_csv(data_file, index_col=[0])[['close','adx']]
 
-import os,sys
-if sys.platform == 'win32' or sys.platform == 'darwin':
-    parralle = True
-    num_workers = 2
-    # number of days we want to pull from the dataframe
-    days_of_data = 7
-    pop_size = 10
-    days = 1
-    generations = 20
-else:
-    parralle = False
-    num_workers = 10
-    days_of_data = 365
-    pop_size = 50
-    days = 7
-    generations = 100
-
-print('days_of_data={},pop_size = {},days = {},generations = {},'.format( days_of_data,pop_size ,days,generations))
 # number of data frames (our DF is in 1h timesteps)
-frames = days_of_data * 24 * 12
-# frames = len(df)
+frames = 7 * 24 * 12 # A WEEK
 train_test_percentage = 0.4
 
 x_train = int(frames * (1 - train_test_percentage))
@@ -34,27 +15,30 @@ print("Friend Shape", df.shape)
 print('train shape', df_train.shape)
 print('test shape', df_test.shape)
 print('columns', df.columns)
-
 print(df_test.head())
 
-del df
-
-import tensortrade
-from neat_stragtegy.neat_trading_strategy import NeatTradingStrategy as TradingStrategy
-from neat_stragtegy.neat_reward_strategy import NeatRewardStrategy as ProfitStrategy
-
+from tensortrade.rewards import RiskAdjustedReturns as ProfitStrategy
 from tensortrade.actions import DiscreteActions
 from tensortrade.exchanges.simulated import SimulatedExchange as Exchange
 from tensortrade.features.scalers import MinMaxNormalizer
 from tensortrade.features import FeaturePipeline
 from tensortrade.environments import TradingEnvironment as Environment
-from tensortrade.trades import Trade
-
 print('fin imports')
-normalize = MinMaxNormalizer(inplace=True,
-                             input_min = 2000,
-                             input_max = 20000)
-feature_pipeline = FeaturePipeline(steps=[normalize])
+
+close_normalize = MinMaxNormalizer(inplace=True,
+                             input_min = 7300,
+                             input_max = 8300,
+                             feature_min = -1,
+                             feature_max = 1,
+                             columns=['close',])
+adx_normalize = MinMaxNormalizer(inplace=True,
+                             input_min = 0,
+                             input_max = 100,
+                             feature_min = -1,
+                             feature_max = 1,
+                             columns=['adx',])
+
+feature_pipeline = FeaturePipeline(steps=[close_normalize,adx_normalize])
 
 reward_scheme = ProfitStrategy()
 action_scheme = DiscreteActions(n_actions=5, instrument='BTC/USDT')
@@ -82,9 +66,7 @@ environment = Environment(exchange=exchange,
 print('fin environment')
 print('')
 
-environment.exchange._balance = 200
-
-print(exchange.balance,environment.exchange._balance)
+print(exchange.data_frame.head())
 
 import neat
 import visualize
@@ -96,70 +78,28 @@ import random
 import time,os
 import math
 
-def derive_action(output):
-    # print(output[0])
-    try:
-        action = int( self._actions / 2 * (1 + math.tanh( output[0] )) )
-    except:
-        print( "*****ERROR IN DERIVE ACTION********", output, self._actions )
-        action = -1
-    return action
 
-def eval_genome(env,genome, config: neat.Config = None):
+def eval_genome(env,genome, config):
     # Initialize the network for this genome
-    net = neat.nn.RecurrentNetwork.create( genome, config )
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
     # calculate the steps and keep track of some intial variables
     steps_completed = 0
-    done = False
-
-    # set inital reward
     fitness = 0.0
-    obs = env._next_observation( Trade( 'N/A', 'hold', 0, 0 ) )
-    # walk all timesteps to evaluate our genome
-    # while (steps is not None and (steps == 0 or steps_completed < (steps))):
+    obs = env.reset()
+
     while (steps_completed < 288):
-        # print('steps_completed:{},_data_frame_window:{}'.format(steps_completed,self._data_frame_window))
-        # activate() the genome and calculate the action output
-
-        output = net.activate( obs )
-
-        # action at current step
-        action = derive_action( output )
-        if action is -1:
-            print( 'BROKEN ACTION', output )
-            fitness = -100000
-            break
-
+        outputs = net.activate( obs )
+        action = np.argmax(outputs)
+        #print(obs,outputs,action)
+        print(net.values,action)
         # feed action into environment to get reward for selected action
-        obs, rewards, done, info = self.environment.step( action )
-
-        # feed rewards to NEAT to calculate fitness.
+        obs, rewards, done, info = environment.step( action )
         fitness += rewards
-
-        # count this as a completed step
         steps_completed += 1
 
-        # stop iterating if we haven't learned to trade or we pass a fitness threshold
-        if fitness < self._learn_to_trade_theshold:
-            if self._watch_genome_evaluation:
-                print( "Learn to trade asshole!" )
-            done = True
-
-        self._genome_performance[genome.key]['rewards'] += rewards
-        self._genome_performance[genome.key]['actions'].append( action )
-        self._genome_performance[genome.key]['steps_completed'] = steps_completed
-        self._genome_performance[genome.key]['trades'] = len( self.environment.exchange.trades )
-        self._genome_performance[genome.key]['balance'] = self.environment.exchange.balance
-        self._genome_performance[genome.key]['net_worth'] = self.environment.exchange.net_worth
-
         if done:
-            if self._watch_genome_evaluation:
-                print( '-------WE DONE!---------' )
             break
 
-    # ballance our reward by how much profit we've made in our trading session.
-
-    self._report_genome_evaluation( genome )
     return fitness
 
 
@@ -178,7 +118,7 @@ class PooledEvaluate(object):
         # print("Evaluating {0} test episodes".format(len(self.test_episodes)))
         if self.pool is None:
             for genome_id, genome in genomes:
-                eval_genome(genome, config)
+                eval_genome(self.env,genome, config)
         else:
             jobs = []
             for genome_id, genome in genomes:
@@ -191,7 +131,7 @@ class PooledEvaluate(object):
         print("final fitness compute time {0}\n".format(time.time() - t0))
 
 
-def run(NUM_CORES):
+def run(NUM_CORES,GENERATIONS):
     # Load the config file, which is assumed to live in
     # the same directory as this script.
     local_dir = os.path.dirname(__file__)
@@ -211,8 +151,9 @@ def run(NUM_CORES):
     # or the user interrupts the process.
     ec = PooledEvaluate(NUM_CORES= NUM_CORES,env = environment)
 
-    gen_best = pop.run( ec.evaluate_genomes, 5 )
+    gen_best = pop.run( ec.evaluate_genomes, GENERATIONS )
 
 if __name__ == '__main__':
-    NUM_CORES = 2
-    run(NUM_CORES)
+    GENERATIONS = 10
+    NUM_CORES = 1
+    run(NUM_CORES,GENERATIONS)
